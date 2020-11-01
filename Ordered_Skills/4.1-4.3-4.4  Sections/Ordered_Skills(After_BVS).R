7fa9cde13bae163dec547fbdea5e2010684cdf1b
Vasilis-Palaskas <67687246+Vasilis-Palaskas@users.noreply.github.com>
2020-11-01 21:11:44 +0200

    Add files via upload

--- Ordered_Skills/4.1-4.3-4.4  Sections/Ordered_Skills(After_BVS).R
+++ Ordered_Skills/4.1-4.3-4.4  Sections/Ordered_Skills(After_BVS).R
@@ -12,7 +12,7 @@ load("datalist_ordered")
 setwd("C:/Users/vasileios palaskas/Desktop/Github folder/Bayesian_Variable_Selection_Volleyball/Ordered_Skills/4.1-4.3-4.4  Sections")
 #Rename the proper names
 
-names(dataList$X)<-c("perfect serve","very good serve","failed serve"," perfect pass","very good pass",
+names(dataList$X)<-c("perfect serve","very good serve","failed serve","perfect pass","very good pass",
                      "poor pass","failed pass","perfect att1","blocked att1",
                      "failed att1","perfect att2","blocked att2","failed att2","perfect block",
                      "block net violation","failed block","failed setting")
@@ -156,3 +156,51 @@ waic(log_lik_ordered_skills_after_BVS_model4)####273.2
 loo(log_lik_ordered_skills_after_BVS_model4)#for model with thin=1
 loo(log_lik_ordered_skills_after_BVS_model4,r_eff=r_eff_log_lik_ordered_skills_after_BVS_model4)#for model with proper thinning 379,9
 DIC_Gelman(deviance_ordered_skills_after_BVS_model4)
+
+
+
+###### SECTION 4.4 Final chosen ordered logistic model with team abilities
+skill_events<-dataList$X
+
+X_ordered_Skills<-skill_events[,colnames(skill_events)%in%
+                                 c("failed serve","failed pass",
+                                   "perfect att1", "failed att1",
+                                   "perfect att2", "failed att2",
+                                   "perfect block","failed setting") ]
+model_data<-list(Y=dataList$Y,X=X_ordered_Skills,n_teams=12,
+                 N=dataList$N,K=ncol(X_ordered_Skills),ncat=6)
+
+ordered_skills_after_BVS<-stan("ordered_skills_after_BVS.stan",iter=14000, warmup=2000,chains=2,thin=2,
+                                      data=model_data,control=list(max_treedepth=15),cores=2)
+
+### MCMC Posterior Summary Plots
+
+
+sims <- rstan::extract(ordered_skills_after_BVS)
+
+## coefplot for skill events differences
+skill_events_differences <-   c("failed serve","failed pass",
+                                "perfect att1", "failed att1",
+                                "perfect att2", "failed att2",
+                                "perfect block","failed setting")
+# teams_index <- unique(dataList$home_team)
+beta <- sims$beta
+beta_hat <- apply(beta,2, median)
+beta_sd <- apply(beta,2, sd)
+#teams_index <- match(squadre16_17, teams)
+# att <- att[,3,teams_index]
+# def <- def[,3,teams_index]
+# att_hat <- apply(att,2,median)
+# att_sd <- apply(att,2,sd)
+# def_hat <- apply(def,2,median)
+# def_sd <- apply(def,2,sd)
+ord <- order(beta_hat, decreasing = TRUE)
+# ord_2 <- order(def_hat)
+
+coefplot( rev(beta_hat[ord]), 
+          rev(beta_sd[ord]), 
+          CI=2, 
+          varnames=rev(as.character(skill_events_differences)[ord]), 
+          main="Skill Events Differences (estim. +/- 2 s.e.)\n", 
+          cex.var=1.5, mar=c(1,6,4.5,1),
+          cex.main=1.3,pch=16, cex=2, col="blue")
