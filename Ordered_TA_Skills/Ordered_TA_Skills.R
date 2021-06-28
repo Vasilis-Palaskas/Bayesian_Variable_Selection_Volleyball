f0f6e0b842aac8c708a3a44f1789d8a58b474fba
Vasileios <vasileios.palaskas@fantasysportsinteractive.co.uk>
2021-06-28 22:01:16 +0300

    zdts ta skills for collinearity

--- Ordered_TA_Skills/Ordered_TA_Skills.R
+++ Ordered_TA_Skills/Ordered_TA_Skills.R
@@ -3,9 +3,14 @@ library(rstan)
 library(coda)
 library(bayesplot)
 library(ggmcmc)
-# Choose the working directory of this file (.../BVS_Paper/Ordered_TA_Skills)
-setwd("C:\\Users\\vasileios palaskas\\Desktop\\Github folder\\Bayesian_Variable_Selection_Volleyball\\Ordered_TA_Skills")
-# Load the properly full prepared data ("datalist_ordered") for the ordered logistic models.
+# Choose the working directory of this file (...\\Submitted_Appendix\\Ordered\\)
+
+#---Data Preparation
+source(file.choose())#-Data_Preparation.R
+
+# Choose the working directory of this file (...\\Submitted_Appendix\\ZDTS\\)
+# setwd("C:/Users/vasileios palaskas/Desktop/Github folder/Bayesian_Variable_Selection_Volleyball/ZDTS_TA_Skills")
+
 #------Skills for both Home and Away Teams
 X_home<-data_by_sets[c(
   "Home_perfect_serves","Home_very_good_serves",
@@ -24,12 +29,6 @@ X_away<-data_by_sets[c(
   "Away_blocked_att2","Away_failed_att2","Away_perfect_blocks",
   "Away_net_violation_blocks","Away_failed_blocks","Away_failed_settings")
 ]
-
-
-
-#----Rename properly the skill variables
-##----Skill events selected via the BVS process based on PSI Median Threshold
-#### Standardization of the Model Matrices for numerical convenience
 
 # Load the properly prepared data ("Data_ordered_skills").
 # load("datalist_ordered")
@@ -77,8 +76,8 @@ teams_points<-paste0(teams," ",observed_points)
 #Numerize the factors in terms of your convenience
 dataList<-list(Y=data_by_sets$sets_difference_factor,X=X_home_diff,n_teams=length(levels(data_by_sets$home_Team)),
                N=dim(data_by_sets)[1],K=ncol(X_home_diff),ncat=6,
-	        home_team=as.numeric(dataList$home_team),
-      away_team=as.numeric(dataList$away_team))
+               home_team=as.numeric(data_by_sets$home_Team),
+               away_team=as.numeric(data_by_sets$away_Team))
 
 
 
@@ -119,14 +118,14 @@ T<-30000 # Total MCMC iterations
 # Step 2  
 for (i in 1:T){
   print(i)
+
   # Step 3: Data input needed for running the model through RStan.
   data_varsel<-list(Y=dataList$Y,X=dataList$X,
-                    home_team=as.numeric(dataList$home_team),
-                    away_team=as.numeric(dataList$away_team),n_teams=dataList$n_teams,
-                    N=dataList$N,K=dataList$K,
+                    N=dataList$N,K=dataList$K,n_teams=dataList$n_teams,
+                    home_team=as.numeric(data_by_sets$home_Team),
+                    away_team=as.numeric(data_by_sets$away_Team),
                     ncat=6,gammas=gammas,post_mean_betas=post_mean_betas,
                     post_sd_betas=post_sd_betas)
-  
   # Step 4:Run the model through RStan for one sampling iteration (20 warm up and 21 total iterations, 21-20=1 sampling iteration) in order to update the betas from the full conditional posterior distributions. 
   # Use the previous iteration's parameter values as initial parameter values so MCMC Algorithm can begin.
   ord_volley_skills_all<-stan("Ordered_BVS_TA_Skills.stan",
--- ZDTS_Skills/ZDTS_Skills.R
+++ ZDTS_Skills/ZDTS_Skills.R
@@ -1,55 +1,66 @@
 # Load the proper libraries.
 library(rstan)
 library(coda)
-library(skellam)
 library(bayesplot)
 library(ggmcmc)
+library(car)
+# Choose the working directory of this file (...\\Submitted_Appendix\\Ordered\\)
+
+#---Data Preparation
+source(file.choose())#-Data_Preparation.R
+
 # Choose the working directory of this file (...\\Submitted_Appendix\\ZDTS\\)
-setwd("C:/Users/vasileios palaskas/Desktop/BVS_Paper/ZDTS_Skills")
-# Load the properly prepared data for both home and away skill events as well as
-# both home and away teams in each match
-load("X_home")
-load("X_away")
-load("data_zdts_skills")
+# setwd("C:/Users/vasileios palaskas/Desktop/Github folder/Bayesian_Variable_Selection_Volleyball/ZDTS_TA_Skills")
 
+#------Skills for both Home and Away Teams
+X_home<-data_by_sets[c(
+  "Home_perfect_serves","Home_very_good_serves",
+  "Home_failed_serves","Home_perfect_passes","Home_very_good_passes",
+  "Home_poor_passes","Home_failed_passes","Home_perfect_att1",
+  "Home_blocked_att1","Home_failed_att1","Home_perfect_att2",
+  "Home_blocked_att2","Home_failed_att2","Home_perfect_blocks",
+  "Home_net_violation_blocks","Home_failed_blocks","Home_failed_settings")
+]
 
+X_away<-data_by_sets[c(
+  "Away_perfect_serves","Away_very_good_serves",
+  "Away_failed_serves","Away_perfect_passes","Away_very_good_passes",
+  "Away_poor_passes","Away_failed_passes","Away_perfect_att1",
+  "Away_blocked_att1","Away_failed_att1","Away_perfect_att2",
+  "Away_blocked_att2","Away_failed_att2","Away_perfect_blocks",
+  "Away_net_violation_blocks","Away_failed_blocks","Away_failed_settings")
+]
 
+# Load the properly prepared data ("Data_ordered_skills").
+# load("datalist_ordered")
 
-#Rename the columns
-colnames(X_home)<-c("(Home) perfect serve","(Home) very good serve","(Home) failed serve","(Home) perfect pass",
-                    "(Home) very good pass","(Home) poor pass","(Home) failed pass","(Home) perfect att1","(Home) blocked att1",
-                    "(Home) failed att1","(Home) perfect att2","(Home) blocked att2","(Home) failed att2","(Home) perfect block",
-                    "(Home) block net violation","(Home) failed block","(Home) failed setting")
-
-colnames(X_away)<-c("(Away) perfect serve","(Away) very good serve","(Away) failed serve","(Away) perfect pass",
-                    "(Away) very good pass","(Away) poor pass","(Away) failed pass","(Away) perfect att1","(Away) blocked att1",
-                    "(Away) failed att1","(Away) perfect att2","(Away) blocked att2","(Away) failed att2","(Away) perfect block",
-                    "(Away) block net violation","(Away) failed block","(Away) failed setting")
-
+X_home_diff<-data.frame(X_home-X_away)
+colnames(X_home_diff)<-c(
+  "perfect_serves","very_good_serves",
+  "failed_serves","perfect_passes","very_good_passes",
+  "poor_passes","failed_passes","perfect_att1",
+  "blocked_att1","failed_att1","perfect_att2",
+  "blocked_att2","failed_att2","perfect_blocks",
+  "net_violation_blocks","failed_blocks","failed_settings")
+#----Rename properly the skill variables
+##----Skill events selected via the BVS process based on PSI Median Threshold
 #### Standardization of the Model Matrices for numerical convenience
-X_home_std<-X_away_std<-matrix(NA,nrow=132,ncol=17)
-for (i in 1:dim(X_home)[2]){
-  X_home_std[,i]<-(X_home[,i]-mean(X_home[,i]))/sd(X_home[,i])
-  X_away_std[,i]<-(X_away[,i]-mean(X_away[,i]))/sd(X_away[,i])
-}
 
-colnames(X_home_std)<-c("(Home) perfect serve","(Home) very good serve","(Home) failed serve","(Home) perfect pass",
-                        "(Home) very good pass","(Home) poor pass","(Home) failed pass","(Home) perfect att1","(Home) blocked att1",
-                        "(Home) failed att1","(Home) perfect att2","(Home) blocked att2","(Home) failed att2","(Home) perfect block",
-                        "(Home) block net violation","(Home) failed block","(Home) failed setting")
+X_home_std<-data.frame(scale(X_home,center=T,scale=T) )
+X_away_std<-data.frame(scale(X_away,center=T,scale=T) )
+X_home_diff_std<-data.frame(scale(X_home-X_away,center=T,scale=T) )
+X_away_diff_std<-data.frame(scale(X_away-X_home,center=T,scale=T) )
 
-colnames(X_away_std)<-c("(Away) perfect serve","(Away) very good serve","(Away) failed serve","(Away) perfect pass",
-                        "(Away) very good pass","(Away) poor pass","(Away) failed pass","(Away) perfect att1","(Away) blocked att1",
-                        "(Away) failed att1","(Away) perfect att2","(Away) blocked att2","(Away) failed att2","(Away) perfect block",
-                        "(Away) block net violation","(Away) failed block","(Away) failed setting")
 
 #-------Step 0: Run the full model to obtain the pilot posterior stv. and means of model parameters
 data_zdts_only_skills<-list(c_thres=2,c_std=5,
-                            n_games=data_zdts_skills$N,
-                       n_teams=data_zdts_skills$n_teams,
+                            n_games=dim(data_by_sets)[1],
+                       n_teams=length(levels(data_by_sets$home_Team)),
                        X_home=X_home_std,X_away=X_away_std,K=ncol(X_home_std),
-                       home_sets=data_zdts_skills$home_sets,
-                       away_sets=data_zdts_skills$away_sets)
+                       home_sets=data_by_sets$home_sets,
+                       away_sets=data_by_sets$away_sets)
+
+
 
 full_zdts_only_skills<-stan(file.choose(),
                              data=data_zdts_only_skills,chains=4,init_r=0.5,
